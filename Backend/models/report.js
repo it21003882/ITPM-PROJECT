08e7427219ed3943c05da2bff32b146b9cf3c499
buddhikaSadun917 <87796498+BuddhikaSadun@users.noreply.github.com>
2023-04-08 09:36:19 +0530

    Added model files

---
+++ Backend/models/report.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const reportSchema = new mongoose.Schema({
+
+    NIC:{
+        type:Number,
+        required:true
+    },
+    ContactNo:{
+        type:String,
+        required:true
+    },
+    WasteType:{
+        type:String,
+        required:true
+    },
+    
+    location:{
+        type:String,
+        required:false
+    },
+    Evidence:{
+        // data:Buffer,
+        //contentType:String
+        type:String,
+        required:false
+     },
+   
+})
+
+
+
+module.exports=mongoose.model('Report',reportSchema);
