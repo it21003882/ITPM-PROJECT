b6cd72f82cad873cd2ddbb96f83e54e7a766b2ed
buddhikaSadun917 <87796498+BuddhikaSadun@users.noreply.github.com>
2023-05-20 01:10:35 +0530

    Added Map & Form component files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Form} from './components/Form.js';
+import {Location} from './components/Location.js';
+import {InsertForm} from "./components/InsertForm.js";
+import {UpdateForm} from "./components/UpdateForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InsertForm/>
+      
     </div>
   );
 }
